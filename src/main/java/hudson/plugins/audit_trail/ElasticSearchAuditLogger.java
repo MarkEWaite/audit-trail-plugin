90e917411e03621e85e7ce71c45fec31815d1e66
Jesper <mr.andersson@njesper.se>
2026-06-09 13:56:52 +0200

    Fix formatting

--- src/main/java/hudson/plugins/audit_trail/ElasticSearchAuditLogger.java
+++ src/main/java/hudson/plugins/audit_trail/ElasticSearchAuditLogger.java
@@ -391,13 +391,23 @@ public class ElasticSearchAuditLogger extends AuditLogger {
                     "@timestamp", DATE_FORMATTER.format(Calendar.getInstance().getTime()));
             payload.put("jenkins.version", Jenkins.VERSION);
             payload.put("jenkins.url", Jenkins.get().getRootUrl());
-            payload.put("jenkins.audittrail.plugin.version", Jenkins.get().pluginManager.getPlugin("audit-trail").getVersion().toString());
+            payload.put(
+                "jenkins.audittrail.plugin.version",
+                Jenkins.get()
+                    .pluginManager
+                    .getPlugin("audit-trail")
+                    .getVersion()
+                    .toString());
             try {
-                    payload.put("jenkins.controller.computer.name", java.net.InetAddress.getLocalHost().getHostName());
-                    payload.put("jenkins.controller.computer.address", java.net.InetAddress.getLocalHost().getHostAddress());
+                    payload.put(
+                        "jenkins.controller.computer.name",
+                        java.net.InetAddress.getLocalHost().getHostName());
+                    payload.put(
+                        "jenkins.controller.computer.address",
+                        java.net.InetAddress.getLocalHost().getHostAddress());
             } catch (Exception e) {
-                    payload.put("jenkins.controller.computer.name", "");
-                    payload.put("jenkins.controller.computer.address", "");
+                payload.put("jenkins.controller.computer.name", "");
+                payload.put("jenkins.controller.computer.address", "");
             }
             StringEntity input = new StringEntity(
                     payload.toString(), ContentType.APPLICATION_JSON, StandardCharsets.UTF_8.name(), false);
